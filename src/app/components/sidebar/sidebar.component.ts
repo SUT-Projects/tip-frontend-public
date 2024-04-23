0b38a2626752e56c18c4f5219d599577fc17d9c9
steven030397 <steven030397@gmail.com>
2024-04-23 18:15:01 +1000

    Quiz interface

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,8 +10,8 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
     { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' }
+    { path: '/tables', title: 'Quizzes',  icon:'ni-bullet-list-67 text-red', class: '' },
+    { path: '/login', title: 'Logout',  icon:'ni-key-25 text-info', class: '' }
 ];
 
 @Component({
