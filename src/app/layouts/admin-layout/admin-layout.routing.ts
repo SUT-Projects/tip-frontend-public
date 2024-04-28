4f0b9804ba50b216beab5bb0b73eea8d63104bd2
steven030397 <steven030397@gmail.com>
2024-04-28 18:55:41 +1000

    remove questions comp (not needed)

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,7 +3,6 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
-import { QuestionsComponent } from 'src/app/questions/questions.component';
 import { QuizComponent } from 'src/app/quiz/quiz.component';
 
 export const AdminLayoutRoutes: Routes = [
